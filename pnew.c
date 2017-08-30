76a5ad71c6121b24f1cfffeb77e2f5fc2eecd0e2
Pritesh Ranjan <pranjan@ncsu.edu>
2017-08-29 21:19:14 -0400

    sdfsd

--- pnew.c
+++ pnew.c
@@ -3,41 +3,34 @@
    #define NPROCS 8
 
    main(int argc, char *argv[])  {
-   int        rank, new_rank, sendbuf, recvbuf, numtasks,
-              ranks1[4]={0,1,2,3}, ranks2[4]={4,5,6,7};
-   MPI_Group  orig_group, new_group;   // required variables
-   MPI_Comm   new_comm;   // required variable
+    int i, j, rank, size, succ, pred;
+    int new_rank, new_size;
+    
+    MPI_Group orig_group, new_group;
+    MPI_Comm  new_comm;
+    MPI_Status status;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    int *new_ranks;
+    new_ranks = (int*)malloc(size * sizeof(int));
+    for (i=0; i< size; i++)
+        new_ranks[i] = i;
+    
+    MPI_Comm_group(MPI_COMM_WORLD, &orig_group);
+    MPI_Group_incl(orig_group, size, new_ranks, &new_group);
+    MPI_Comm_create(MPI_COMM_WORLD, new_group, &new_comm);
+    
+    MPI_Comm_rank(new_comm, &new_rank);
+    MPI_Comm_size(new_comm, &new_size);
+    
+    printf("\n rank = %d, size = %d, new rank= %d, size= %d",rank, size, new_rank, new_size);
 
-   MPI_Init(&argc,&argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-
-   if (numtasks != NPROCS) {
-     printf("Must specify MP_PROCS= %d. Terminating.\n",NPROCS);
-     MPI_Finalize();
-     exit(0);
-     }
-
-   sendbuf = rank;
-
-   // extract the original group handle
-   MPI_Comm_group(MPI_COMM_WORLD, &orig_group);
-
-   //  divide tasks into two distinct groups based upon rank
-   if (rank < NPROCS/2) {
-     MPI_Group_incl(orig_group, NPROCS/2, ranks1, &new_group);
-     }
-   else {
-     MPI_Group_incl(orig_group, NPROCS/2, ranks2, &new_group);
-     }
-
-   // create new new communicator and then perform collective communications
-   MPI_Comm_create(MPI_COMM_WORLD, new_group, &new_comm);
-   MPI_Allreduce(&sendbuf, &recvbuf, 1, MPI_INT, MPI_SUM, new_comm);
-
-   // get rank in new group
-   MPI_Group_rank (new_group, &new_rank);
-   printf("rank= %d newrank= %d recvbuf= %d\n",rank,new_rank,recvbuf);
+    succ = (rank+1) % size;
+    pred = (rank-1 + size) % size;
 
    MPI_Finalize();
+   return 0;
    }
