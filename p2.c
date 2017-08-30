a172ac5e759c5c9da277e243e794074921c7cc33
Pritesh Ranjan <pranjan@ncsu.edu>
2017-08-30 12:15:17 -0400

    Refactoring started

--- p2.c
+++ p2.c
@@ -17,6 +17,8 @@
 #define SEND_TO_PRED 0
 #define RECV_FROM_SUCC 0
 #define ROOT 0
+#define DEBUG 0
+#define VERBOSE 1
 
 double fn(double x) {
     return pow(x,3) - pow(x,2) - x + 1;
@@ -47,53 +49,57 @@ void create_new_communicator(int *rank, int *size, MPI_Comm *new_comm) {
 
 }
 
-int main(int argc, char *argv[]) {
-    // loop index, process identity, number of processes.
-    int i, j, rank, size, succ, pred;
-
-    MPI_Comm  new_comm;
-    MPI_Status status;
-
-    MPI_Init(&argc, &argv);
-    create_new_communicator(&rank, &size, &new_comm);
+void get_x_axis_limits(int rank, int size, int *n_ngrid, int *start_x, int *end_x, int *succ, int *pred) {
+    /* Utility Function to calculate the decomposed grid's limits on X axis.
     
-    succ = (rank+1) % size;
-    pred = (rank-1 + size) % size;
-    // domain array and step size
-    double x[NGRID +2], dx;
-    int n_ngrid;  // Number of grid points allocated to the process.
-    int start_x, end_x; // start and end index for x values for this process.
-    double block_size = NGRID / size;  // Number of grid points in a block
-    
-    // Last process is responsible for the extra grid points, which will 
-    // not be more than size extra grid points.
-    if (rank == size-1) { 
-        n_ngrid = (NGRID / size) + (NGRID % size);
-        start_x = rank * block_size;
-        end_x = start_x + n_ngrid;
-
+    Last process is responsible for the extra grid points, which will 
+    not be more than size extra grid points.
+    */
+    if (rank == size-1) {
+        *n_ngrid = (NGRID/size) + (NGRID % size);
+        *start_x = rank * (NGRID/size);
+        *end_x = *start_x + *n_ngrid;
     } else {
-        n_ngrid = (NGRID / size);
-        start_x = rank * block_size;
-        end_x = start_x + n_ngrid;
+        *n_ngrid = (NGRID/size);
+        *start_x = rank * (NGRID/size);
+        *end_x = *start_x + *n_ngrid;
     }
+    *succ = (rank+1) % size;
+    *pred = (rank-1 + size) % size;
+}
 
-    double  local_min_max[DEGREE-1];
-    int min_max_count = 0;
-    double *y, *dy, *err;
-
-    double *glo_err= NULL;
-    printf("\nRank= %d, size= %d, start_x= %d, end_x= %d, n_ngrid=%d, ", rank, size, start_x, end_x, n_ngrid );
-   
-    // Create the grid points on X axis
-    for (i=1; i <=NGRID ; i++) {
+double create_x_axis_grid_points(double *x) {
+    int i=1;
+    double dx;
+    
+    for(i=1; i <= NGRID; i++) {
         x[i] = XI + (XF - XI) * (double)(i-1)/(double)(NGRID-1);
     }
     dx = x[2] - x[1];
-    x[0] = x[1] - dx;  
-    x[NGRID+1] = x[NGRID] + dx; 
+    x[0] = x[1] - dx;
+    x[NGRID+1] = x[NGRID] + dx;
+    return dx;
+}
 
-    printf("succ= %d, pred= %d,  dx= %f",succ, pred, dx);
+void blocking_and_manual_reduce(int rank, int size, MPI_Comm new_comm) {
+    int i, j, succ, pred;
+    double x[NGRID +2], dx;
+    int n_ngrid;  // Number of grid points allocated to the process.
+    int start_x, end_x; // start and end index for x values for this process.
+    int block_size = NGRID / size;  // Number of grid points in a block
+    
+    get_x_axis_limits(rank, size, &n_ngrid, &start_x, &end_x, &succ, &pred);
+    dx = create_x_axis_grid_points(x);
+    if(VERBOSE) printf("\nRank= %d, size= %d, start_x= %d, end_x= %d, "
+                 "n_ngrid=%d, succ= %d, pred= %d,  dx= %f ",rank, size, 
+                  start_x, end_x, n_ngrid,succ, pred, dx);
+    
+    double  local_min_max[DEGREE-1];
+    int min_max_count = 0;
+    double *y=NULL, *dy=NULL, *err=NULL, *glo_err= NULL;
+    double err_sum, std_dev, err_avg, *glo_min_max=NULL;
+
+
     y = (double *) malloc((n_ngrid +2) * sizeof(double));
     dy = (double *) malloc(n_ngrid * sizeof(double));
     err = (double *) malloc(n_ngrid * sizeof(double));
@@ -105,14 +111,17 @@ int main(int argc, char *argv[]) {
     for (i=start_x, j=1; i< end_x; i++, j++) {
         y[j] = fn(x[i]);
     }
-    //TODO: send y[1] to predecessor
-    //      send y[n_ngrid] to successor
-    //      Receive in y[0] from predeessor
-    //      Receive in y[n_ngrid+1] from successor.
-    //
-
+    
+    /*
+    send y[1] to predecessor
+    send y[n_ngrid] to successor
+    Receive in y[0] from predeessor
+    Receive in y[n_ngrid+1] from successor.
+    */
     if(rank == ROOT) { // Root has no predecessor
         glo_err = (double *)malloc(NGRID  * sizeof(double));
+        glo_min_max = (double *)malloc(((DEGREE-1)*size) * sizeof(double));
+
         // ROOT sends to rank 1, with tag SEND_TO_SUCC and receives with tag RECV_FROM_SUCC 
         y[0] = fn(x[0]-dx);
         MPI_Send(&y[n_ngrid], 1, MPI_DOUBLE, succ, SEND_TO_SUCC, new_comm);
@@ -146,23 +155,22 @@ int main(int argc, char *argv[]) {
             dy[j-1] = (y[j+1] - y[j-1]) / (2*dx);
         }
     }
-    printf("\n%d No deadlock occured\n", rank); 
     
+    if(VERBOSE) printf("\n%d No deadlock occured\n", rank); 
     for(i=start_x, j=0; j < n_ngrid; i++, j++) {
         err[j] = fabs(dy[j] - dfn(x[i]));
         
         if(fabs(dy[j]) < EPSILON) {
             local_min_max[min_max_count++] = x[i];
-           printf("\nProcess %d found MIN/MAX at x= %f , dy= %f ",rank, x[i], dy[j]);
+            if(VERBOSE) printf("\nProcess %d found MIN/MAX at x= %f , dy= %f ",rank, x[i], dy[j]);
         }
-        //printf("%d %d %d %8f, %8f, %8f, %8f, %8f, %e\n", rank, i, j, x[i], fn(x[i]), y[j], dfn(x[i]), dy[j], err[j]);
-        //printf("%d %d %d %8f %10e\n",rank, i, j, x[i], err[j]);
     }
+    
     for(j=min_max_count; j<DEGREE-1; j++){
             local_min_max[j] = INT_MAX;
     }
-    //TODO: Send this error vector to ROOT
-    int st=0, *rcounts, *displs;
+
+    int *rcounts=NULL, *displs=NULL;
     rcounts = (int *)malloc(size * sizeof(int));
     displs = (int *)malloc(size * sizeof(int));
 
@@ -173,12 +181,13 @@ int main(int argc, char *argv[]) {
     rcounts[size-1] = block_size + (NGRID % size);
     displs[size-1] = (size-1)*block_size;
 
-    st = MPI_Gatherv(err, n_ngrid , MPI_DOUBLE, glo_err, rcounts, displs, MPI_DOUBLE, ROOT, new_comm);
-    double err_sum, std_dev, err_avg;
-    if (rank == ROOT)
-    {   err_sum = std_dev = 0;
+    MPI_Gatherv(err, n_ngrid , MPI_DOUBLE, glo_err, rcounts, displs, MPI_DOUBLE, ROOT, new_comm);
+    MPI_Gather(local_min_max, DEGREE-1, MPI_DOUBLE, glo_min_max, DEGREE-1, MPI_DOUBLE, ROOT, new_comm);
+
+    if (rank == ROOT) {
+        err_sum = std_dev = 0;
         for (i=0; i< NGRID; i++) {
-             //printf("\ni=%d , x= %f, err= %e", i, x[i], glo_err[i]);
+             if(DEBUG) printf("\ni=%d , x= %f, err= %e", i, x[i], glo_err[i]);
              err_sum += glo_err[i];
         }
         err_avg = err_sum / (double)NGRID;
@@ -186,29 +195,41 @@ int main(int argc, char *argv[]) {
             std_dev += pow(glo_err[i] - err_avg, 2);
         }
         std_dev = sqrt(std_dev/(double)NGRID);
-        printf("\nErr_sum = %e, Err_avg = %e, std_dev= %e",err_sum, err_avg, std_dev);
-    }
-    double * glo_min_max=NULL;
-    if(rank == ROOT) {
-        glo_min_max = (double *)malloc(((DEGREE-1)*size) * sizeof(double));
-    }
-    st = MPI_Gather(local_min_max, DEGREE-1, MPI_DOUBLE, glo_min_max, DEGREE-1, MPI_DOUBLE, ROOT, new_comm);
-    if (rank == ROOT) {
+        if(VERBOSE) printf("\nErr_sum = %e, Err_avg = %e, std_dev= %e",err_sum, err_avg, std_dev);
+    
         for(i=0; i< (DEGREE-1)*size; i++)
             if(glo_min_max[i] != INT_MAX) {
-               printf("\n(%f, %f)",glo_min_max[i], fn(glo_min_max[i]));
+               if(VERBOSE)printf("\n(%f, %f)",glo_min_max[i], fn(glo_min_max[i]));
             }
     }
-    if (st != MPI_SUCCESS) {
-        printf("\nError in MPI_GATHER %d\n",st);
-        exit(0);
-    }
-    
-    MPI_Finalize();
-    
     if(y) free(y);
     if(dy) free(dy);
     if(err) free(err);
+    if(displs) free(displs);
     if(glo_err) free(glo_err);
+    if(rcounts) free(rcounts);
+    if(glo_min_max) free(glo_min_max);
+}
+
+void blocking_and_MPI_reduce() {
+}
+
+void non_blocking_and_manual_reduce() {
+}
+
+void non_blocking_and_MPI_reduce() {
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+
+    MPI_Comm  new_comm;
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    create_new_communicator(&rank, &size, &new_comm);
+    blocking_and_manual_reduce(rank, size, new_comm);
+   
+    MPI_Finalize();
     return 0;
 }
